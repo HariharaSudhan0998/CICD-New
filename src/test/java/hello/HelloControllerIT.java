a37dd5bfa9f63b3628ea574f6d62c5ed22c68909
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-17 11:30:53 +0530

    Update HelloControllerIT.java

--- src/test/java/hello/HelloControllerIT.java
+++ src/test/java/hello/HelloControllerIT.java
@@ -14,7 +14,10 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerIT {
