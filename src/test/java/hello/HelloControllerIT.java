3a4cc3979b0df21b046f1c2164fa590867c62d3c
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-17 11:30:13 +0530

    Update HelloControllerIT.java

--- src/test/java/hello/HelloControllerIT.java
+++ src/test/java/hello/HelloControllerIT.java
@@ -33,9 +33,21 @@ public class HelloControllerIT {
     }
 
     @Test
-    public void getHello() throws Exception {
+   /* public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity(base.toString(),
                 String.class);
         assertThat(response.getBody(), equalTo(" Welcome to Flogile Technologies!"));
+    }*/
+    
+    public void getHello()  {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("headless");
+options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
     }
 }
